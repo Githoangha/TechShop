02496dedc7993c570e40a93ed737bb8b880cf278
ha hoang <hoanghadqhk15@gmail.com>
2024-01-13 20:43:16 +0700

    Update

---
+++ EcommerceShop/Domain/entities/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Abstractrions
+{
+    class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
