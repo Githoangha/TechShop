7ae8b4a789f59e3cde31a8bf7ca2eb3fa04b2bb6
ha hoang <hoanghadqhk15@gmail.com>
2024-01-14 17:17:47 +0700

    update

--- EcommerceShop/Domain/entities/ApplicationDbContext.cs
+++ EcommerceShop/Domain/entities/ApplicationDbContext.cs
@@ -3,10 +3,16 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Domain.Abstractrions
+namespace Domain.Entities
 {
     class ApplicationDbContext : DbContext
     {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<Bill> Bills { get; set; }
+        public DbSet<BillDetail> BillDetails { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
